9fa08272baaba02a0c12c5abd99c69fa17186bda
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-05-15 00:17:11 -0400

    added static data to its own file

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,40 +1,8 @@
-import { Container, Typography } from '@mui/material'
-
-import { useContext } from 'react';
-import { Context } from '../context/Context';
+import { Container } from '@mui/material'
 import CarouselCard from './CarouselCard';
+import StaticHotelData from "../data/StaticData"
 
 const Carousel = () => {
-
-  const { roomList } = useContext(Context);
-
-  // let list = roomList.slice(0, 3);
-
-  const list = [
-    {
-      id: "304142",
-      name: "Bluegreen's Bayside Resort",
-      image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/407991210.jpg?k=2ebb3aea5cf6ef2121923a559719b48ad631eeb90dda9f4f6f9adbb92ee7465b&o=&hp=1",
-      description: "Bluegreen's Bayside Resort and Spa at Panama City Beach has a terrace. Guests at the resort will be able to enjoy activities in and around Panama City Beach, like hiking, fishing and cycling.",
-      bookingLink: "https://www.booking.com/hotel/us/bluegreen-39-s-bayside-resort-and-spa-at-panama-city-beach.html?label=gen173nr-1FCAEoggI46AdIM1gEaI8CiAEBmAExuAEXyAEM2AEB6AEB-AECiAIBqAIDuAKq2pCyBsACAdICJDhiNjdhM2NmLTExOWEtNGExYy05NTBhLTViYjBjYmFhYjlmZtgCBeACAQ&aid=304142&ucfs=1&arphpl=1&dest_id=20023560&dest_type=city&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=12&hapos=12&sr_order=popularity&srpvid=71f01921c3d700b4&srepoch=1715744898&from=searchresults#hotelTmpl"
-
-    },
-    {
-      id: "304142",
-      name: "Hilton San Francisco",
-      image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/474351488.jpg?k=d922a3b395ecc64d4002631930cd9b9b3b67f15501fc32992af2c0e84ac408a2&o=&hp=1",
-      description: "Located in the heart of downtown San Francisco, this Hilton is conveniently located crossroads of the Financial District, Chinatown, and North Beach. It features an on-site restaurant and bar and an award-winning concierge service.",
-      bookingLink: "https://www.booking.com/hotel/us/hilton-san-francisco-financial-district.html?aid=304142&label=gen173nr-1FCAEoggI46AdIM1gEaI8CiAEBmAExuAEXyAEM2AEB6AEB-AECiAIBqAIDuAKq2pCyBsACAdICJDhiNjdhM2NmLTExOWEtNGExYy05NTBhLTViYjBjYmFhYjlmZtgCBeACAQ&sid=f3592d85993bf19f761eae8458d70023&dest_id=20015732&dest_type=city&dist=0&group_adults=2&group_children=0&hapos=9&hpos=9&no_rooms=1&req_adults=2&req_children=0&room1=A%2CA&sb_price_type=total&sr_order=popularity&srepoch=1715744604&srpvid=fd801a2111640179&type=total&ucfs=1&activeTab=main"
-    },
-    {
-      id: "304142",
-      name: "ModernHaus SoHo",
-      image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/297840629.jpg?k=d20e005d5404a7bea91cb5fe624842f72b27867139c5d65700ab7f69396026ce&o=&hp=1",
-      description: "Rooms at ModernHaus SoHo boast a floor-to-ceiling glass wall separating the bathroom from the living quarters. Each room includes wood floors, 42-inch flat-screen TVs, Nespresso coffee, and Le Labo bath amenities. Wifi is included.",
-      bookingLink: "https://www.booking.com/hotel/us/modernhaus-soho.html?aid=304142&label=gen173nr-1FCAEoggI46AdIM1gEaI8CiAEBmAExuAEXyAEM2AEB6AEB-AECiAIBqAIDuAKq2pCyBsACAdICJDhiNjdhM2NmLTExOWEtNGExYy05NTBhLTViYjBjYmFhYjlmZtgCBeACAQ&sid=f3592d85993bf19f761eae8458d70023&dest_id=20088325&dest_type=city&dist=0&group_adults=2&group_children=0&hapos=17&hpos=17&no_rooms=1&req_adults=2&req_children=0&room1=A%2CA&sb_price_type=total&sr_order=class&srepoch=1715745046&srpvid=07861afc61ce00cb&type=total&ucfs=1&activeTab=main"
-
-    }
-  ]
 
   return (
     <Container
@@ -47,7 +15,7 @@ const Carousel = () => {
         marginY: '50px'
       }}
     >
-      {list && list.map(hotel => (
+      {StaticHotelData && StaticHotelData.map(hotel => (
         <CarouselCard key={hotel.id} hotel={hotel} />
       ))}
     </Container>
