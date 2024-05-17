254642b459f5f8143c7c16f1b61d873af5305ef0
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-05-17 14:15:45 -0400

    a

--- src/axios/api.js
+++ src/axios/api.js
@@ -5,24 +5,25 @@ const BASE_URL = process.env.REACT_APP_API_URL;
 const api = axios.create({
     baseURL: BASE_URL,
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
     }
 })
 
 const _get = (url, config = {}) => {
-    return api.get(url, config);
+  return api.get(url, config);
 }
+
 const _delete = (url, config = {}) => {
-    return api.delete(url, config);
-  };
-  
-  const _put = (url, data = {}, config = {}) => {
-    return api.put(url, data, config);
-  };
-  
-  const _post = (url, data = {}, config = {}) => {
-    return api.post(url, data, config);
-  };
+  return api.delete(url, config);
+};
+
+const _put = (url, data = {}, config = {}) => {
+  return api.put(url, data, config);
+};
+
+const _post = (url, data = {}, config = {}) => {
+  return api.post(url, data, config);
+};
   
   // Export API methods
   export { _get, _delete, _put, _post }
